b47cd55640f885e650d6a3d8f17c3df675db5761
jiangchen <cjiang@86links.com>
2021-07-13 19:45:10 +0800

    lock support

--- sample-code/src/main/java/tech/jiangchen/concurrent/LockSupportDemo3.java
+++ sample-code/src/main/java/tech/jiangchen/concurrent/LockSupportDemo3.java
@@ -3,22 +3,16 @@ package tech.jiangchen.concurrent;
 import java.util.concurrent.locks.LockSupport;
 
 /**
- * 如果线程在 park 上受阻塞，则它将解除其阻塞状态。否则，保证下一次调用 park 不会受阻塞。如果给定线程尚未启动，则无法保证此操作有任何效果
+ * interrupt起到的作用与unpark一样
  *
  * @author jiangchen
  * @date 2021/07/13
  */
-public class LockSupportDemo2 {
+public class LockSupportDemo3 {
 
     public static void main(String[] args) throws InterruptedException {
         MyThread myThread = new MyThread(Thread.currentThread());
         myThread.start();
-        try {
-            // 主线程睡眠3s
-            Thread.sleep(3000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
         System.out.println("before park");
         // 获取许可
         LockSupport.park("ParkAndUnparkDemo");
@@ -34,10 +28,17 @@ public class LockSupportDemo2 {
         }
 
         public void run() {
-            System.out.println("before unpark");
-            // 释放许可
-            LockSupport.unpark((Thread) object);
-            System.out.println("after unpark");
+            System.out.println("before interrupt");
+            try {
+                // 休眠3s
+                Thread.sleep(3000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            Thread thread = (Thread) object;
+            // 中断线程
+            thread.interrupt();
+            System.out.println("after interrupt");
         }
     }
 
